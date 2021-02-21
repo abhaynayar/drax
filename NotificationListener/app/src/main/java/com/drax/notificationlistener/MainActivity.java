55fce4819abfe9557514078ad2ea049f72302844
abhaynayar <me@abhaynayar.com>
2021-02-21 04:45:00 +0100

    NotificationListener: Added sqlite support

--- NotificationListener/app/src/main/java/com/drax/notificationlistener/MainActivity.java
+++ NotificationListener/app/src/main/java/com/drax/notificationlistener/MainActivity.java
@@ -11,6 +11,8 @@ import android.content.IntentFilter;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
@@ -19,27 +21,61 @@ import android.provider.Settings;
 import android.util.Log;
 import android.view.View;
 import java.util.Calendar;
+import java.util.Date;
+
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.ScrollView;
 
+// TODO: Remove duplicate logs.
 // TODO: Save logs to persistent storage.
-// TODO: Use table instead of csv.
-// TODO: Add timestamp to logs.
 // TODO: Display most recent log at the top.
+// TODO: Self-notification to test if notifications are still being logged.
 
 public class MainActivity extends AppCompatActivity {
-    private static final String LOG_TAG = "NLS";
+    public static final String TAG = "NLS";
+    public static SQLiteDatabase db;
+    static TextView tvLog;
+
+    public static void asdf(String asdf) {
+        tvLog.setText(asdf);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tvLog = (TextView) findViewById(R.id.tvLog);
+
+        // Getting some essential permissions:
+        getBatteryAccess();
+        getNotificationAccess();
+
+        // Create DB:
+        db = openOrCreateDatabase("nls", MODE_PRIVATE, null);
+        db.execSQL("CREATE TABLE IF NOT EXISTS log(ts TEXT, package TEXT, title TEXT, text TEXT);");
+        db.execSQL("DELETE FROM log;");
+
+        // Share logs:
+        Button btnSave = (Button) findViewById(R.id.btnShare);
+        btnSave.setOnClickListener(v -> {
+            Intent shareIntent = new Intent(Intent.ACTION_SEND);
+            shareIntent.setType("text/plain");
+            shareIntent.putExtra(Intent.EXTRA_TEXT, tvLog.getText());
+            shareIntent.putExtra(Intent.EXTRA_SUBJECT, "Notification log " + Calendar.getInstance().getTime());
+            startActivity(Intent.createChooser(shareIntent, "Share..."));
+        });
+    }
 
     void getNotificationAccess() {
         // Ask for notification access permission.
         NotificationManager nm = (NotificationManager) getApplicationContext()
                 .getSystemService(Context.NOTIFICATION_SERVICE);
         if (!nm.isNotificationPolicyAccessGranted()) {
-            Log.i(LOG_TAG, "[-] Notification access denied.");
+            Log.i(TAG, "[-] Notification access denied.");
             startActivity(new Intent(Settings.ACTION_NOTIFICATION_LISTENER_SETTINGS));
         } else {
-            Log.i(LOG_TAG, "[+] Notification access granted.");
+            Log.i(TAG, "[+] Notification access granted.");
         }
     }
 
@@ -49,55 +85,10 @@ public class MainActivity extends AppCompatActivity {
         String packageName = this.getPackageName();
         PowerManager pm = (PowerManager) this.getSystemService(Context.POWER_SERVICE);
         if (!pm.isIgnoringBatteryOptimizations(packageName)) {
-            Log.e(LOG_TAG, "[-] Not ignoring battery optimization.");
+            Log.e(TAG, "[-] Not ignoring battery optimization.");
             startActivity(new Intent(Settings.ACTION_IGNORE_BATTERY_OPTIMIZATION_SETTINGS));
         } else {
-            Log.i(LOG_TAG, "[+] Ignoring battery optimization.");
-        }
-    }
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        // Get some essential permissions:
-        getBatteryAccess();
-        getNotificationAccess();
-
-        // Listen for broadcasts by the notification listener.
-        IntentFilter intentFilter = new IntentFilter();
-        intentFilter.addAction("com.drax.nls");
-        registerReceiver(new SomeBroadcastReceiver(), intentFilter);
-
-        // Share notifications log:
-        Button btnSave = (Button) findViewById(R.id.btnShare);
-        btnSave.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent shareIntent = new Intent(Intent.ACTION_SEND);
-                shareIntent.setType("text/plain");
-                TextView tvNotifications = (TextView) findViewById(R.id.tvNotifications);
-                shareIntent.putExtra(Intent.EXTRA_TEXT,tvNotifications.getText());
-                shareIntent.putExtra(Intent.EXTRA_SUBJECT, "Notification log " + Calendar.getInstance().getTime());
-                startActivity(Intent.createChooser(shareIntent, "Share..."));
-            }
-        });
-    }
-
-    static String aggregation = "";
-    public class SomeBroadcastReceiver extends BroadcastReceiver {
-        @Override
-
-        public void onReceive(Context context, Intent intent) {
-
-            // Concatenate notification contents from the broadcast.
-            String nc = intent.getExtras().getString("notificationContent");
-            aggregation += nc + "\n";
-
-            // Update TextView with the concatenated result.
-            TextView tvNotifications = (TextView) findViewById(R.id.tvNotifications);
-            tvNotifications.setText(aggregation);
+            Log.i(TAG, "[+] Ignoring battery optimization.");
         }
     }
 }
--- NotificationListener/app/src/main/java/com/drax/notificationlistener/MyListener.java
+++ NotificationListener/app/src/main/java/com/drax/notificationlistener/MyListener.java
@@ -1,29 +1,53 @@
 package com.drax.notificationlistener;
 
-import android.content.Intent;
-import android.os.Bundle;
 import android.service.notification.NotificationListenerService;
 import android.service.notification.StatusBarNotification;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import java.util.Calendar;
+import java.util.Date;
+
+import static com.drax.notificationlistener.MainActivity.TAG;
+import static com.drax.notificationlistener.MainActivity.db;
 
 public class MyListener extends NotificationListenerService {
 
     @Override
     public void onNotificationPosted(StatusBarNotification sbn){
-        Intent intent = new  Intent("com.drax.nls");
+        String title="", text="";
 
-        String all = "";
-        all += sbn.getPackageName() + ", ";
+        Date currentTime = Calendar.getInstance().getTime();
         Bundle extras = sbn.getNotification().extras;
 
         if(extras.getString("android.title") != null){
-            all += extras.getString("android.title") + ", ";
+            title = extras.getString("android.title");
         }
 
         if(extras.getCharSequence("android.text") != null){
-            all += extras.getCharSequence("android.text").toString();
+            text += extras.getCharSequence("android.text").toString();
         }
 
-        intent.putExtra("notificationContent", all);
-        sendBroadcast(intent);
+        db.execSQL("INSERT INTO log VALUES('" + currentTime + "', '" + sbn.getPackageName() + "', '"
+                                                           + title + "', '" + text + "');");
+
+        String tableString="";
+        Cursor allRows  = db.rawQuery("SELECT * FROM log;", null);
+        if (allRows.moveToFirst() ){
+            String[] columnNames = allRows.getColumnNames();
+            do {
+                for (String name: columnNames) {
+                    tableString += String.format("%s: %s\n", name,
+                            allRows.getString(allRows.getColumnIndex(name)));
+                }
+                tableString += "\n";
+
+            } while (allRows.moveToNext());
+        }
+
+        Log.i(TAG, tableString);
+        MainActivity.asdf(tableString);
     }
 }
